8afca762ebe832a4bafbfa5c9422a24f89fd7f67
Monica <51380067+LuongMonica@users.noreply.github.com>
2021-11-28 20:22:53 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const MAL_API_ANIME = require("./src/Mal_Api_Authorized/Mal_Api_Anime/Mal_Api_Anime");
 const MAL_API_LIST_ANIME = require("./src/Mal_Api_Authorized/Mal_Api_List_Anime/Mal_Api_List_Anime");
-const MAL_OAUTH = require("./src/Mal_Oauth/Mal_Oauth");
 
 exports.API_ANIME = MAL_API_ANIME;
 exports.API_LIST_ANIME = MAL_API_LIST_ANIME;
-exports.OAUTH = MAL_OAUTH;
